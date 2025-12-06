5db08855e374801bc2305cb68025bfee25bb7e52
huok <77536370+dee-shar@users.noreply.github.com>
2025-12-07 00:15:03 +0300

    Refactor object classes to inherit from BaseModel

--- src/utils/objects.py
+++ src/utils/objects.py
@@ -1,215 +1,126 @@
-class ChatThreads:
-	def __init__(self, data):
-		self.json = data
-		self.title = []
-		self.ndc_id = []
-		self.content = []
-		self.thread_id = []
-	
-	@property
-	def ChatThreads(self):
-		for thread in self.json:
-			try:
-				self.title.append(thread["title"])
-			except (KeyError, TypeError):
-				pass
-			try:
-				self.content.append(thread["content"])
-			except (KeyError, TypeError):
-				pass
-			try:
-				self.thread_id.append(thread["threadId"])
-			except (KeyError, TypeError):
-				pass
-			try:
-				self.ndc_id.append(thread["ndcId"])
-			except (KeyError, TypeError):
-				pass
-		return self
+class BaseModel:
+    @staticmethod
+    def safe_get(object: dict, *keys, default: str = None):
+        try:
+            for key in keys:
+                object = object[key]
+            return object
+        except (KeyError, TypeError):
+            return default
 
+class ChatThreads(BaseModel):
+    def __init__(self, data: list) -> None:
+        self.json = data
+        self.title: list = []
+        self.ndc_id: list = []
+        self.content: list = []
+        self.thread_id: list = []
 
-class CommunityList:
-	def __init__(self, data):
-		self.json = data
-		self.name = []
-		self.link = []
-		self.ndc_id = []
-		self.amino_id = []
+    def parse(self):
+        for thread in self.json:
+            self.title.append(self.safe_get(thread, "title"))
+            self.content.append(self.safe_get(thread, "content"))
+            self.thread_id.append(self.safe_get(thread, "threadId"))
+            self.ndc_id.append(self.safe_get(thread, "ndcId"))
+        return self
 
-	@property
-	def CommunityList(self):
-		for community in self.json:
-			self.name.append(community["name"])
-			self.link.append(community["link"])
-			self.ndc_id.append(community["ndcId"])
-			self.amino_id.append(community["endpoint"])
-		return self
+class CommunityList(BaseModel):
+    def __init__(self, data: list) -> None:
+        self.json = data
+        self.name: list = []
+        self.link: list = []
+        self.ndc_id: list = []
+        self.amino_id: list = []
 
-class MembersList:
-	def __init__(self, data):
-		self.json = data
-		self.icon = []
-		self.user_id = []
-		self.nickname = []
-		self.created_time = []
-	
-	@property
-	def MembersList(self):
-		for members in self.json:
-			try:
-				self.nickname.append(members["nickname"])
-			except (KeyError, TypeError):
-				pass
-			try:
-				self.user_id.append(members["uid"])
-			except (KeyError, TypeError):
-				pass
-			try:
-				self.created_time.append(members["createdTime"])
-			except (KeyError, TypeError):
-				pass
-			try:
-				self.icon.append(members["icon"])
-			except (KeyError, TypeError):
-				pass
-		return self
+    def parse(self):
+        for community in self.json:
+            self.name.append(self.safe_get(community, "name"))
+            self.link.append(self.safe_get(community, "link"))
+            self.ndc_id.append(self.safe_get(community, "ndcId"))
+            self.amino_id.append(self.safe_get(community, "endpoint"))
+        return self
 
-class FromLink:
-	def __init__(self, data):
-		self.json = data
-		self.path = None
-		self.ndc_id = None
-		self.full_url = None
-		self.object_id = None 
-		self.short_url = None 
-		self.full_path = None  
-		self.short_code = None
-		self.object_type = None 
-		self.target_code = None 
-	
-	@property
-	def FromLink(self):
-		try:
-			self.path = self.json["path"] 
-		except (KeyError, TypeError): 
-			pass		
-		try:
-			self.object_type = self.json["extensions"]["linkInfo"]["objectType"]
-		except (KeyError, TypeError): 
-			pass
-		try: 
-			self.short_code = self.json["extensions"]["linkInfo"]["shortCode"]
-		except (KeyError, TypeError):
-			pass
-		try:
-			self.full_path = self.json["extensions"]["linkInfo"]["fullPath"]
-		except (KeyError, TypeError):
-			pass
-		try:
-			self.target_code = self.json["extensions"]["linkInfo"]["targetCode"]
-		except (KeyError, TypeError):
-			pass
-		try: 
-			self.object_id = self.json["extensions"]["linkInfo"]["objectId"]
-		except (KeyError, TypeError):
-			pass
-		try:
-			self.short_url = self.json["extensions"]["linkInfo"]["shareURLShortCode"]
-		except (KeyError, TypeError):
-			pass
-		try:
-			self.full_url = self.json["extensions"]["linkInfo"]["shareURLFullPath"]
-		except (KeyError, TypeError):
-			pass
-		try:
-			self.ndc_id = self.json["extensions"]["linkInfo"]["ndcId"]
-		except (KeyError, TypeError):
-			pass
-		return self
+class MembersList(BaseModel):
+    def __init__(self, data: list)-> None:
+        self.json = data
+        self.icon: list = []
+        self.user_id: list = []
+        self.nickname: list = []
+        self.created_time: list = []
 
-class UserInfo:
-	def __init__(self, data):
-		self.json = data
-		self.icon = None
-		self.web_URL = None
-		self.user_id = None
-		self.content = None 
-		self.amino_id = None 
-		self.nickname = None
-		self.created_time = None 
-		self.modified_time = None
+    def parse(self):
+        for member in self.json:
+            self.nickname.append(self.safe_get(member, "nickname"))
+            self.user_id.append(self.safe_get(member, "uid"))
+            self.created_time.append(self.safe_get(member, "createdTime"))
+            self.icon.append(self.safe_get(member, "icon"))
+        return self
 
-	@property
-	def UserInfo(self):
-		try:
-			self.amino_id = self.json["aminoId"]
-		except (KeyError, TypeError):
-			pass 
-		try:
-			self.user_id = self.json["uid"]
-		except (KeyError, TypeError): 
-			pass 
-		try:
-			self.nickname = self.json["nickname"]
-		except (KeyError, TypeError): 
-			pass 
-		try:
-			self.content = self.json["content"]
-		except (KeyError, TypeError): 
-			pass 
-		try:
-			self.icon = self.json["icon"]
-		except (KeyError, TypeError): 
-			pass 
-		try:
-			self.web_URL = self.json["webURL"]
-		except (KeyError, TypeError): 
-			pass
-		try:
-			self.created_time = self.json["createdTime"]
-		except (KeyError, TypeError): 
-			pass 
-		try:
-			self.modified_time = self.json["modifiedTime"]
-		except (KeyError, TypeError): 
-			pass 
-		return self 
-		
-class BlogsList:
-	def __init__(self, data):
-		self.json = data
-		self.title = []
-		self.blog_id = []
-		self.content = []
-		self.created_time = []
-		self.modified_time = []
-		self.comments_count = []
-		
-	@property
-	def BlogsList(self):
-		for blog in self.json:
-			try:
-				self.blog_id.append(blog["blogId"])
-			except (KeyError, TypeError):
-				pass
-			try:
-				self.title.append(blog["title"])
-			except (KeyError, TypeError):
-				pass
-			try:
-				self.content.append(blog["content"])
-			except (KeyError, TypeError):
-				pass
-			try:
-				self.comments_count.append(blog["commentsCount"])
-			except (KeyError, TypeError):
-				pass
-			try:
-				self.created_time.append(blog["createdTime"])
-			except (KeyError, TypeError):
-				pass
-			try:
-				self.modified_time.append(blog["modifiedTime"])
-			except (KeyError, TypeError):
-				pass
-		return self
+class FromLink(BaseModel):
+    def __init__(self, data: dict) -> None:
+        self.json = data
+        self.path: str | None = None
+        self.ndc_id: int | None = None
+        self.full_url: str | None = None
+        self.object_id: str | None = None
+        self.short_url: str | None = None
+        self.full_path: str | None = None
+        self.short_code: str | None = None
+        self.object_type: int | None = None
+        self.target_code: int | None = None
+
+    def parse(self):
+        extensions = ("extensions", "linkInfo")
+        self.path = self.safe_get(self.json, "path")
+        self.object_type = self.safe_get(self.json, *extensions, "objectType")
+        self.short_code = self.safe_get(self.json, *extensions, "shortCode")
+        self.full_path = self.safe_get(self.json, *extensions, "fullPath")
+        self.target_code = self.safe_get(self.json, *extensions, "targetCode")
+        self.object_id = self.safe_get(self.json, *extensions, "objectId")
+        self.short_url = self.safe_get(self.json, *extensions, "shareURLShortCode")
+        self.full_url = self.safe_get(self.json, *extensions, "shareURLFullPath")
+        self.ndc_id = self.safe_get(self.json, *extensions, "ndcId")
+        return self
+
+class UserInfo(BaseModel):
+    def __init__(self, data: dict) -> None:
+        self.json = data
+        self.icon: str | None = None
+        self.web_URL: str | None = None
+        self.user_id: str | None = None
+        self.content: str | None = None
+        self.amino_id: str | None = None
+        self.nickname: str | None = None
+        self.created_time: str | None = None
+        self.modified_time: str | None = None
+
+    def parse(self):
+        self.amino_id = self.safe_get(self.json, "aminoId")
+        self.user_id = self.safe_get(self.json, "uid")
+        self.nickname = self.safe_get(self.json, "nickname")
+        self.content = self.safe_get(self.json, "content")
+        self.icon = self.safe_get(self.json, "icon")
+        self.web_URL = self.safe_get(self.json, "webURL")
+        self.created_time = self.safe_get(self.json, "createdTime")
+        self.modified_time = self.safe_get(self.json, "modifiedTime")
+        return self
+
+class BlogsList(BaseModel):
+    def __init__(self, data: list) -> None:
+        self.json = data
+        self.title: list = []
+        self.blog_id: list = []
+        self.content: list = []
+        self.created_time: list = []
+        self.modified_time: list = []
+        self.comments_count: list = []
+
+    def parse(self):
+        for blog in self.json:
+            self.blog_id.append(self.safe_get(blog, "blogId"))
+            self.title.append(self.safe_get(blog, "title"))
+            self.content.append(self.safe_get(blog, "content"))
+            self.comments_count.append(self.safe_get(blog, "commentsCount"))
+            self.created_time.append(self.safe_get(blog, "createdTime"))
+            self.modified_time.append(self.safe_get(blog, "modifiedTime"))
+        return self
